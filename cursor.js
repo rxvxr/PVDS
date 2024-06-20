65694db1e54e8963c33a1c53af7807d173089def
river <63168443+rxvxr@users.noreply.github.com>
2024-06-20 15:02:12 +0200

    Add files via upload

---
+++ cursor.js
@@ -0,0 +1,14 @@
+const cursorBig = document.querySelector('.big');
+
+
+const positionElement = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+   
+  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  
+  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+}
+
+window.addEventListener('mousemove', positionElement)
